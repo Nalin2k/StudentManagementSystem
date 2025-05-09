10ff59e1c99a0cafafcd00e5d29710fde3384ef5
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-05-09 11:58:45 +0530

    Application Complete

--- src/main/java/com/nalin/StudentManagementSystem/service/StudentService.java
+++ src/main/java/com/nalin/StudentManagementSystem/service/StudentService.java
@@ -8,4 +8,11 @@ public interface StudentService {
 	
 	public List<Student> getAllStudents();
 	
+	public Student saveStudent(Student student);
+	
+	public Student getById(int id);
+	
+	public void deleteById(int id);
 }
+
+	
